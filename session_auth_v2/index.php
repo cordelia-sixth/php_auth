263896bb23f4eefddb416d1df314326ceb782ffc
cordelia <80014084+cordelia-sixth@users.noreply.github.com>
2021-05-09 23:25:08 +0900

    Create index.php

---
+++ session_auth_v2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>ログイン</title>
+</head>
+<body>
+  <p>ログイン</p>
+
+  <form action="check.php" method="post">
+    name:<input type="text" name="name"><br>
+    pass:<input type="text" name="pass">
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
